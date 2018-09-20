b483ed1820eedf36e23b811c50c587b2994309cf
EdenaRuh <diego.rios.ctag@gmail.com>
2018-09-20 19:50:37 +0200

    update

--- sint48P2.java
+++ sint48P2.java
@@ -5,21 +5,34 @@ public class Sint48P2 extends HttpServlet {
 public void doGet(HttpServletRequest req, HttpServletResponse res)
 throws IOException, ServletException
 {
-res.setContentType("text/html");
+res.setContentType("text/html ; charset=UTF-8");
 PrintWriter out = res.getWriter();
-out.println("<html>");
-out.println("<head>");
-out.println("<title>Sint: Práctica 2. Consulta de canciones</title>");
-out.println("</head>");
-out.println("<body>");
-out.println("<h1>Servicio de consulta de canciones</h1>");
-out.println("<h2>Bienvenido a este servicio</h2>");
-/*out.println("<a href = "">Pulsa aquí para ver los ficheros erróneos</a>");*/
-out.println("<h3>Selecciona una consulta:</h3>");
-out.println("<input type = \"radio\" checked>Consulta1: Canciones de un interprete que duran menos que una dada</input>");
-out.println("<br></br>");
-out.println("<input type = \"submit\"></input>");
-out.println("</body>");
-out.println("</html>");
+String fase = req.getParameter("pfase");
+String anio = req.getParameter("anio");
+String disco = req.getParameter("disco");
+String cancion = req.getParameter("cancion");
+switch(fase)
+{
+    case "01": inicio(out); break;
 }
+}
+public void inicio(PrintWriter out, String fase)
+{
+    out.println("<html>");
+    out.println("<head>");
+    out.println("<title>Sint: Práctica 2. Consulta de canciones</title>");
+    out.println("</head>");
+    out.println("<body>");
+    out.println("<h1>Servicio de consulta de canciones</h1>");
+    out.println("<h2>Bienvenido a este servicio.</h2>");
+    out.println("<form name = \"miforminit\">");
+    out.println("<a href = \"Pulsa aqui para ver los ficheros erroneos\" onclick = 'document.miforminit.action=\"?fase02\"'></a>");
+    out.println("<h3>Selecciona una consulta:</h3>");
+    out.println("<input type = \"radio\" checked>Consulta1: Canciones de un interprete que duran menos que una dada</input>");
+    out.println("<br></br>");
+    out.println("<input type = \"submit\"></input>");
+    out.println("<input type = \"hidden\" name = \"pfase\"></input>");
+    out.println("</body>");
+    out.println("</html>");
+} 
 }
