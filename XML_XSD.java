e91bc187072ff8d3d343b478d7ef13b04385eacc
EdenaRuh <diego.rios.ctag@gmail.com>
2018-10-15 10:45:26 +0200

    update

--- XML_XSD.java
+++ XML_XSD.java
@@ -21,7 +21,7 @@ public class XML_XSD
     static String JAXP_SCHEMA_LANGUAGE = "http://java.sun.com/xml/jaxp/properties/schemaLanguage";
     static String W3C_XML_SCHEMA = "http://www.w3.org/2001/XMLSchema";
     static String JAXP_SCHEMA_SOURCE = "http://java.sun.com/xml/jaxp/properties/schemaSource";
-    static String MY_SCHEMA = "C:/Users/Diego/Desktop/clasessint/iml.xsd";
+    static String MY_SCHEMA = "iml.xsd";
     public static XML_XSD_ErrorHandler error=null;
     public static String url;
 
@@ -29,10 +29,11 @@ public class XML_XSD
         //Creada batería de parsers
         DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
         //Asignación de atributos
-        dbf.setValidating(false); //Para DTD true. Por defecto ya está a false.
+        dbf.setValidating(true); //Para DTD true. Por defecto ya está a false.
         dbf.setNamespaceAware(true);
         dbf.setAttribute(JAXP_SCHEMA_LANGUAGE, W3C_XML_SCHEMA);
-        dbf.setAttribute(JAXP_SCHEMA_SOURCE, MY_SCHEMA);        
+        dbf.setAttribute(JAXP_SCHEMA_SOURCE, MY_SCHEMA);
+        //dbf.setSchema(Schema);        
         DocumentBuilder db = null;
         Document doc = null;
         LinkedList<String> files = new LinkedList<String>();
@@ -50,7 +51,8 @@ public class XML_XSD
         }catch(ParserConfigurationException e){
             e.printStackTrace();
         }
-        files.add("http://gssi.det.uvigo.es/users/agil/public_html/SINT/18-19/iml2001.xml");
+        files.add("iml2001.xml");
+        //files.add("http://gssi.det.uvigo.es/users/agil/public_html/SINT/18-19/iml2001.xml");
         while(!files.isEmpty())
         {
             url = (String) files.getFirst();
@@ -60,25 +62,30 @@ public class XML_XSD
                     //Generacion del arbol DOM tras el parseo. Generará un error el método parse si el documento no  es well-formed. Una SAXException
                 //Saltará la clase de gestión de errores en caso de que los contenga (no válido según el schema xml definido).            
                     doc = db.parse(url);
+                    //System.out.print("Esta parseando");
                 }catch(SAXException e){
                     e.printStackTrace();
+                    //System.out.print("Salto la primera");
                 }catch(IOException e){
                     e.printStackTrace();
                 }catch(Exception e){
                     e.printStackTrace();
                 }
                 if(error.hasError())
-                {
+                {                    
                     if(!error.Warning().equals(""))
                     {
+                        //System.out.print("Hay warning");
                         errores.add(error.Warning());
                     }
                     if(!error.Error().equals(""))
                     {
+                        //System.out.print("Hay errores");
                         errores.add(error.Error());
                     }
                     if(!error.fatalError().equals(""))
                     {
+                        //System.out.print("Hay errores fatales");
                         errores.add(error.fatalError());
                     }
                 }
@@ -118,19 +125,19 @@ class XML_XSD_ErrorHandler extends DefaultHandler {
     {
         err = true;
         strWarning = "Warning: "+spe.toString(); 
-        //System.out.println("Warning: "+spe.toString()); 
+        System.out.println("Warning: "+spe.toString()); 
     }
     public void error (SAXParseException spe) throws SAXException 
     { 
         err = true;
         strError = "Error: "+spe.toString();
-        //System.out.println("Error: "+spe.toString()); 
+        System.out.println("Error: "+spe.toString()); 
     }
     public void fatalerror (SAXParseException spe) throws SAXException 
     {
         err = true;
         strFatalError = "Fatal Error: "+spe.toString();
-        //System.out.println("Fatal Error: "+spe.toString());
+        System.out.println("Fatal Error: "+spe.toString());
     }
     public boolean hasError()
     {
